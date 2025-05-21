a95764dfae8cd2433f910e46570759f2c37185cd
Ananya Trivedi <ananyarashmitrivedi@gmail.com>
2025-05-21 11:04:49 +0530

    Add files via upload

---
+++ Fructus/App/FructusApp.swift
@@ -0,0 +1,25 @@
+import SwiftUI
+
+@main
+struct FructusApp: App
+{
+    
+    @AppStorage("isOnboarding") var isOnboarding: Bool = true
+    
+    var body: some Scene
+    {
+        WindowGroup
+        {
+            
+            if isOnboarding
+            {
+                OnboardingView()
+            }
+            else
+            {
+                ContentView()
+            }
+            
+        }
+    }
+}
